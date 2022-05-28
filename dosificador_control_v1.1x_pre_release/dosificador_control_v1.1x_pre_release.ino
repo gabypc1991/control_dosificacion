824c7de7b08baf19ef6a00dd9c12e217ac1e4017
Gabriel Perez Castro <96443097+gabypc1991@users.noreply.github.com>
2022-05-28 19:19:12 -0300

    Update dosificador_control_v1.1x_pre_release.ino

--- dosificador_control_v1.1x_pre_release/dosificador_control_v1.1x_pre_release.ino
+++ dosificador_control_v1.1x_pre_release/dosificador_control_v1.1x_pre_release.ino
@@ -911,14 +911,14 @@ void accion_calibrar(){
   
   lcd.setCursor(11,2); lcd.print(balanza);lcd.print("   ");
   
-  if (pulsacion == '1'){factor_temp +=5;}
-  if (pulsacion == '2'){factor_temp -=5;}
+  if (pulsacion == '1'){factor_temp +=10;}
+  if (pulsacion == '2'){factor_temp -=10;}
 
-  if (pulsacion == '4'){factor_temp +=10;}
-  if (pulsacion == '5'){factor_temp -=10;}
+  if (pulsacion == '4'){factor_temp +=100;}
+  if (pulsacion == '5'){factor_temp -=100;}
 
-  if (pulsacion == '7'){factor_temp +=100;}
-  if (pulsacion == '8'){factor_temp -=100;}
+  if (pulsacion == '7'){factor_temp +=1000;}
+  if (pulsacion == '8'){factor_temp -=1000;}
   
   lcd.setCursor(7,0); lcd.print(factor_temp);
   
